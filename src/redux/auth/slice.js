d454385ddef896d4fb1ecc72f50aa8e06aeb46cd
TetianaChehrina <3909139@gmail.com>
2024-06-18 20:30:38 +0200

    auth changed

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -50,7 +50,8 @@ const authSlice = createSlice({
         state.isRefreshing = true;
       })
       .addCase(refreshUser.fulfilled, (state, action) => {
-        state.user = action.payload.user;
+        state.user = action.payload;
+        state.isLoggedIn = true;
         state.isRefreshing = false;
       }),
 });
